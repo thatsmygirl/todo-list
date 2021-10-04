87adf276d7a26981a6619db8cfc27e59e9022b4b
JHyeonJin <noppi5101@gmail.com>
2021-10-04 22:15:30 +0900

    투두리스트

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,30 +1,32 @@
-import React, { Component } from 'react';
-import './TodoItem.css';
+import React, { Component } from "react";
+import "./TodoItem.css";
 
 class TodoItem extends Component {
+  shouldComponentUpdate(nextProps, nextState) {
+    return this.props.checked !== nextProps.checked;
+  }
 
-    shouldComponentUpdate(nextProps, nextState) {
-        return this.props.checked !== nextProps.checked;
-    }
+  render() {
+    const { text, checked, id, onToggle, onRemove } = this.props;
 
-    render() {
-        const { text, checked, id, onToggle, onRemove } = this.props;
-
-        return (
-            <div className="todo-item" onClick={() => onToggle(id)}>
-                <div className="remove" onClick={(e) => {
-                    e.stopPropagation();
-                    onRemove(id)}
-                }>&times;</div>
-                <div className={`todo-text ${checked && 'checked'}`}>
-                    <div>{text}</div>
-                </div>
-                {
-                    checked && (<div className="check-mark">&#x2713;</div>)
-                }
-            </div>
-        );
-    }
+    return (
+      <div className="todo-item" onClick={() => onToggle(id)}>
+        <div
+          className="remove"
+          onClick={(e) => {
+            e.stopPropagation();
+            onRemove(id);
+          }}
+        >
+          &times;
+        </div>
+        <div className={`todo-text ${checked && "checked"}`}>
+          <div>{text}</div>
+        </div>
+        {checked && <div className="check-mark">&#x2713;</div>}
+      </div>
+    );
+  }
 }
 
 export default TodoItem;
